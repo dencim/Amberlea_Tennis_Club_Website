7f7795097da772f35c4a64994e3b8ce0e7190b2e
dencim <43787288+dencim@users.noreply.github.com>
2021-07-04 16:01:05 -0400

    Add message for registartion down time

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  const TODAY = new Date();
+  const START_REGISTRATION_OUTAGE = new Date(2021, 7, 10);
+  const END_REGISTRATION_OUTAGE = new Date(2021, 07, 22);
+
+  console.log(TODAY);
+
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
@@ -14,6 +20,11 @@ $(document).ready(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".modal").toggleClass("is-active");
   });
+
+  if (TODAY >= START_REGISTRATION_OUTAGE && TODAY <= END_REGISTRATION_OUTAGE) {
+    $(".closed-notification-message").removeClass("invisible");
+    console.log("In outage");
+  }
 });
 
 //.attr('id', '' + i + j);
