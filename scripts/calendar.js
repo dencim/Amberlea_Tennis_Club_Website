d3b851e0126e60b2e08412a4141784fade824fd7
dencim <43787288+dencim@users.noreply.github.com>
2021-07-04 17:49:11 -0400

    Update July Calendar. Add August Calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -43,7 +43,7 @@ const JULY = {
   daysBeforeStart: "4",
   daysInMonth: 31,
   1: "Canada Day",
-  2: "MIXED SENIOR \n DOUBLES \n 8-10 am",
+  2: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   3: "",
   4: "",
   5: `<b>NIKE SUMMER CAMP 9-3:30
@@ -68,26 +68,62 @@ const JULY = {
   1 court 9-10/1-2 </b>`,
   10: "",
   11: "",
-  12: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am `,
+  12: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   13: `ADULT TENNIS <br/> 7-10 pm `,
   14: `WED AM TENNIS <br/> 9-11 am`,
   15: "",
-  16: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am`,
+  16: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   17: "",
   18: "",
-  19: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am `,
+  19: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   20: `ADULT TENNIS <br/> 7-10 pm `,
   21: `WED AM TENNIS <br/> 9-11 am`,
   22: "",
-  23: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am`,
+  23: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   24: "",
   25: "",
-  26: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am `,
+  26: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   27: `ADULT TENNIS <br/> 7-10 pm `,
   28: `WED AM TENNIS <br/> 9-11 am`,
   29: "",
-  30: `MIXED SENIOR <br/> DOUBLES <br/> 8-10 am`,
+  30: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
   31: "",
+};
+
+const AUGUST = {
+  daysBeforeStart: "0",
+  daysInMonth: 31,
+  1: "",
+  2: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  3: `Adult House <br/> League 7-10 pm`,
+  4: `WED AM TENNIS <br/> 9-11 am`,
+  5: ``,
+  6: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  7: ``,
+  8: ``,
+  9: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  10: `Adult House <br/> League 7-10 pm`,
+  11: `WED AM TENNIS <br/> 9-11 am`,
+  12: ``,
+  13: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  14: ``,
+  15: "",
+  16: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  17: `Adult House <br/> League 7-10 pm`,
+  18: `WED AM TENNIS <br/> 9-11 am`,
+  19: ``,
+  20: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  21: ``,
+  22: "",
+  23: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  24: `Adult House <br/> League 7-10 pm`,
+  25: `WED AM TENNIS <br/> 9-11 am`,
+  26: ``,
+  27: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  28: ``,
+  29: "",
+  30: `MIXED SENIOR <br/> DOUBLES <br/> 8-9:30 am <b>2 courts</b> <br/> 9:30-11am <b>1 court </b>`,
+  31: `Adult House <br/> League 7-10 pm`,
 };
 
 $(document).ready(function () {
@@ -96,7 +132,9 @@ $(document).ready(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $("#july").addClass("is-active");
     $("#june").removeClass("is-active");
+    $("#august").removeClass("is-active");
     $(".june-calendar").addClass("invisible");
+    $(".august-calendar").addClass("invisible");
     $(".july-calendar").removeClass("invisible");
   });
 
@@ -105,8 +143,21 @@ $(document).ready(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $("#june").addClass("is-active");
     $("#july").removeClass("is-active");
+    $("#august").removeClass("is-active");
     $(".july-calendar").addClass("invisible");
+    $(".august-calendar").addClass("invisible");
     $(".june-calendar").removeClass("invisible");
+  });
+
+  // Display August calendar
+  $("#august").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $("#august").addClass("is-active");
+    $("#july").removeClass("is-active");
+    $("#june").removeClass("is-active");
+    $(".july-calendar").addClass("invisible");
+    $(".june-calendar").addClass("invisible");
+    $(".august-calendar").removeClass("invisible");
   });
 
   //june
@@ -137,5 +188,20 @@ $(document).ready(function () {
     }
 
     return emptyDays + daysHtml;
+  });
+
+  //august
+  $(".days-august").append(() => {
+    //empty days
+    //let emptyDays = "<li></li>";
+    //emptyDays = emptyDays.repeat(AUGUST.daysBeforeStart); //DONT NEED BECAUSE 0 DAYS
+
+    //full days
+    let daysHtml = "";
+    for (let i = 1; i <= AUGUST.daysInMonth; i++) {
+      daysHtml += `<li> ${i} <p>${AUGUST[i]}</p></li>`;
+    }
+
+    return daysHtml;
   });
 });
